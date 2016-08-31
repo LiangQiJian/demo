60c971ebb066909e0a7f6d3ae2645c7491b07621
xcl <xcl@xcldemacbook-air.local>
2016-08-31 20:18:06 +0800

    Initial Commit

---
+++ demo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  demo
+//
+//  Created by xcl on 16/8/31.
+//  Copyright © 2016年 xcl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
